d21caa0a3088bea0f2e9dd4a3e72076d787f8db4
Pacana-9S-ay2526 <pacana.kl@obmontessori.edu.ph>
2026-02-10 08:51:20 +0800

    Add welcome alert to area calculator

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
+window.alert ("Welcome to MJS Area Radius Calculator!!!!");
+
 function area() {
       var rad = document.getElementById("radius").value;
       var area = Math.PI * (rad ** 2);
       document.getElementById("area").value = area;
+
    }
